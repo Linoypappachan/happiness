88cdcf341bbbba37e5324f40006924c1335dd950
EPOST\malakkaran.pappachan <malakkaran.pappachan@epg.gov.ae>
2018-03-13 13:48:23 +0400

    initial commit for faisal innokat

---
+++ SourceCode/ui/src/app/config/globals.ts
@@ -0,0 +1,11 @@
+var ctxpath = '';
+var service_url_prefix = '';
+
+
+/// #if build
+ctxpath = '/test_happinesscard';
+/// #else
+service_url_prefix = 'http://localhost:3000'
+/// #endif 
+
+export { ctxpath, service_url_prefix };
